346f6f0caf1c9f344fa5f8d6ccd731b04a847d21
Columbus Dong <crdong@ncsu.edu>
2020-02-01 15:39:43 -0500

    Updates to the Board

--- poopoo/Assets/Scripts/ChangeOrder.cs
+++ poopoo/Assets/Scripts/ChangeOrder.cs
@@ -7,8 +7,11 @@ public class ChangeOrder : MonoBehaviour
 {   
     // Text variable for the order
     public Text order;
-    public int difficulty;
-    public int swordsOrdered;
+    // Keep track of the difficulty, swords ordered, and the status [Start|In-Progress][T/F]
+    public static int difficulty;
+    public static int swordsOrdered;
+    public static bool start;
+    // Change the text
     private string text;
 
     // Start is called before the first frame update
@@ -16,29 +19,42 @@ public class ChangeOrder : MonoBehaviour
     {
         order.text = "Taking Orders";
         text = "";
-        StartCoroutine(updateOrder());
+        start = false;
+        difficulty = 0;
+        swordsOrdered = 0;
     }
 
     // Update is called once per frame
+    /**
+        Update the order log when its ready to accept a new order
+    */
     void Update()
-    {
+    {   
+        StartCoroutine(updateOrder());
+    }
 
+    public static void genOrder() {
+        start = true;
     }
 
     // Testing dynamically updating chalk board
     IEnumerator updateOrder() {
-        yield return new WaitForSeconds(5);
-
-        // If the difficult is the default
-        if (difficulty == 0) {
-            difficulty = Random.Range(1, 5);
+        if (Input.GetKeyDown("s")) {
+            start = true;
         }
 
-        // If the swords ordered is the default
-        if (swordsOrdered == 0) {
+        yield return new WaitForSeconds(1);
+
+
+
+        if (start) {
+            // If the difficult is the default
+            difficulty = Random.Range(1, 5);
+
+            // If the swords ordered is the default
             swordsOrdered = Random.Range(1, 10);
         }
-
+            
         /* 
             Print the swords ordered in format:
                   6
@@ -58,8 +74,6 @@ public class ChangeOrder : MonoBehaviour
         }
 
         order.text = text;
-
-
-
+        start = false;
     }
 }
