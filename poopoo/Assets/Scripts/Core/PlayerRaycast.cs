7d82a0825cd6341a06d4d867f691ac4efc74d354
Griffin Geiger <griffin2420@yahoo.com>
2020-02-01 14:17:45 -0500

    Fizzix arms

---
+++ poopoo/Assets/Scripts/Core/PlayerRaycast.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerRaycast : MonoBehaviour
+{
+    public float distanceToSee = 10f;
+    RaycastHit whatWasHit;
+    public Text interactionPrompt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.cyan);
+        if (Physics.Raycast(this.transform.position, this.transform.forward,out whatWasHit, distanceToSee))
+        {
+            
+            GameObject focusedObject = whatWasHit.collider.gameObject;
+            if (focusedObject.CompareTag("Interactable") )
+            {
+                interactionPrompt.text = "Press 'E' to Interact";
+
+            }
+            else if(focusedObject.CompareTag("Grabable"))
+            {
+                interactionPrompt.enabled = true;
+                interactionPrompt.text = "Click to pick up " + focusedObject.name;
+                if(Input.GetKey(KeyCode.Mouse0))
+                {
+                    Destroy(focusedObject);
+                }
+            }
+            else
+            {
+                interactionPrompt.enabled = false;
+            }
+        }
+        else
+        {
+            interactionPrompt.enabled = false;
+        }
+    }
+}
---
+++ poopoo/Assets/Scripts/Core/RigidbodyAnchoring.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Take our own rigidbody and bring it closer to an object
+public class RigidbodyAnchoring : MonoBehaviour
+{
+    public Transform Object;
+    public float force;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce((rb.position - Object.position) * force);
+    }
+
+
+
+
+}
