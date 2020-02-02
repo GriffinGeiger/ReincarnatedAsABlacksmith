648393d9d82db034ae37e016d9507e9faf50dcdf
saeverha <saeverha@ncsu.edu>
2020-02-01 21:31:53 -0500

    sword is stuck when on anvil

---
+++ poopoo/Assets/Scripts/SwordLock.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordLock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider sword)
+    {
+        Debug.Log(sword);
+        if (sword.gameObject.tag == "Sword")
+        {
+            sword.gameObject.layer = 10;
+            Debug.Log("parented");
+            
+            // Debug.Log("Collision");
+        }
+    }
+
+    private void OnTriggerExit(Collider sword)
+    {
+        if (sword.gameObject.tag == "Sword")
+        {
+            sword.gameObject.layer = 11;
+        }
+        
+    }
+}
